2a2f8d90a6fb5c44ef9a8feac7b3b91f0fda7275
Ethan Tuning <etuning5@hotmail.com>
2017-04-05 22:04:25 -0700

    Add files via upload
    Here is my first try. It's not pretty but its works.

---
+++ CSCD437/Assignment 1/Ethan's Solution/src/Quine1.java
@@ -0,0 +1,9 @@
+public class Quine1{
+	public static void main(String ... args){
+		String str = "public class Quine1{%c%cpublic static void main(String ... args){%c%c%cString str = %c%s%c%c%c%cchar quote = 34;%c%c%cchar tab = 9;%c%c%cchar nl = 10;%c%c%cSystem.out.println(str,nl,tab,nl,tab,tab,quote,str,quote,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);%c%c}%c}";
+		char quote = 34;
+		char tab = 9;
+		char nl = 10;
+		System.out.printf(str,nl,tab,nl,tab,tab,quote,str,quote,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);
+	}
+}
