6e95f6c62dac25fcda991cdaa773da7cd352cec0
Ethan Tuning <etuning5@hotmail.com>
2017-04-07 09:41:07 -0700

    Semicolon Issue
    Fixed the issue that the semicolon after the String str declaration was not being printed when ran.

--- CSCD437/Assignment 1/Ethan's Solution/src/Quine1.java
+++ CSCD437/Assignment 1/Ethan's Solution/src/Quine1.java
@@ -1,9 +1,10 @@
 public class Quine1{
 	public static void main(String ... args){
-		String str = "public class Quine1{%c%cpublic static void main(String ... args){%c%c%cString str = %c%s%c%c%c%cchar quote = 34;%c%c%cchar tab = 9;%c%c%cchar nl = 10;%c%c%cSystem.out.printf(str,nl,tab,nl,tab,tab,quote,str,quote,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);%c%c}%c}";
+		String str = "public class Quine1{%c%cpublic static void main(String ... args){%c%c%cString str = %c%s%c%c%c%c%cchar quote = 34;%c%c%cchar tab = 9;%c%c%cchar nl = 10;%c%c%cchar semicol = 59;%c%c%cSystem.out.printf(str,nl,tab,nl,tab,tab,quote,str,quote,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);%c%c}%c}";
 		char quote = 34;
 		char tab = 9;
 		char nl = 10;
-		System.out.printf(str,nl,tab,nl,tab,tab,quote,str,quote,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);
+		char semicol = 59;
+		System.out.printf(str,nl,tab,nl,tab,tab,quote,str,quote,semicol,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,tab,nl,tab,nl);
 	}
 }
