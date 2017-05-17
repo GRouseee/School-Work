d9ec7dbf6bf05580c2ae222d2373247fd3289e7a
Ethan Tuning <etuning5@hotmail.com>
2017-05-16 21:37:59 -0700

    Add files via upload

--- CSCD437/Assignment 6/Java Solution/DefendTheCode.java
+++ CSCD437/Assignment 6/Java Solution/DefendTheCode.java
@@ -11,10 +11,21 @@ import javax.crypto.spec.PBEKeySpec;
 
 public class DefendTheCode{
 	static Scanner kb = new Scanner(System.in);
+	static String password = null;
+	static int int1 = 0;
+	static int int2 = 0;
+	static int addResult = 0;
+	static int multResult = 0;
 	
 	public static void main(String[] args){
 		try{
 			getPassword();
+			getInteger();
+			getInteger();
+			
+			System.out.println(password);
+			System.out.println(addResult);
+			System.out.println(multResult);
 		}catch(Exception e){
 			System.out.println("Something bad happened...");
 		}
@@ -22,24 +33,52 @@ public class DefendTheCode{
 	
 	static boolean getInput(String regex, String prompt, String inputType) throws NoSuchAlgorithmException, InvalidKeySpecException{
 		boolean isValid = false;
+		int intCount = 0;
 		
 		while(!isValid){
 			System.out.println(prompt);
 			String attempt = kb.nextLine();
-			isValid = true;
 			
 			if(inputType.equals("password") && compareToRegex(regex, attempt)){
-				getSecuredPassword(attempt, getSalt());
+				password = getSecuredPassword(attempt, getSalt()).toString();
 				isValid = true;
+			}
+			
+			if(inputType.equals("integer") && compareToRegex(regex, attempt)){
+				intCount++;
+				int1 = Integer.parseInt(attempt);
+				
+				if(intCount == 2){
+					int2 =Integer.parseInt(attempt);
+					addResult = addInts(int1, int2);
+					multResult = multInts(int1, int2);
+					isValid = true;
+				}
 			}
 		}
 		
 		return isValid;
 	}
 	
+	static int addInts(int num1, int num2){
+		return num1 + num2;
+	}
+	
+	static int multInts(int num1, int num2){
+		return num1 * num2;
+	}
+	
+	static boolean getInteger() throws NoSuchAlgorithmException, InvalidKeySpecException{
+		String intRegex = "\\d{1}";
+		String prompt = "Enter an integer value: ";
+		boolean isValid = getInput(intRegex, prompt, "integer");
+		
+		return isValid;
+	}
+	
 	static boolean getPassword() throws NoSuchAlgorithmException, InvalidKeySpecException{
 		String passwordRegex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
-		String prompt = "Enter a password at least 8 characters long (must contain a lower case, upper case, a digit, and a special character)";
+		String prompt = "Enter a password at least 8 characters long (must contain a lower case, upper case, a digit, and a special character): ";
 		boolean isValid = getInput(passwordRegex, prompt, "password");
 		
 		return isValid;
@@ -71,9 +110,9 @@ public class DefendTheCode{
 	}
 	
 	static boolean compareToRegex(String regex, String input){
-        	Pattern pattern = Pattern.compile(regex);
-        	Matcher matcher = pattern.matcher(input);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
         
-        	return matcher.matches();
+        return matcher.matches();
 	}
 }
